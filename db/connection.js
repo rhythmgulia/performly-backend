f49430c114374a8500f50f863f97f12b3903246f
“nidhiisinghh” <nidhiisinghh2007@gmail.com>
2025-05-18 18:43:34 +0530

    Updated and added the schemas.

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); 
 
-const mongoURL = process.env.mongoURL || "mongodb://localhost:27017/practice"; 
+const mongoURL = process.env.MONGO_URL || "mongodb://localhost:27017/practice"; 
 mongoose.connect(mongoURL)
     .then(() => console.log("Connected to database."))
     .catch((err) => console.error("Database connection error:", err));
--- db/models/client.js
+++ db/models/client.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const ClientSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
