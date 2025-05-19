e24f89905b69186a2e02d5ca180b1d8b7d641bc8
Nidhi Singh <nidhiisinghh2007@gmail.com>
2025-05-19 11:13:59 +0530

    Update client.js

--- backend/db/models/client.js
+++ backend/db/models/client.js
@@ -4,16 +4,12 @@ const mongoose = require('mongoose');
 const ClientSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'users', // was 'User'
+        ref: 'users', 
         required: true
     },
-    favorites: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Performer'
-    }],
     bookings: [{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'bookings' // was 'Booking'
+        ref: 'bookings' 
     }],
 });
 
