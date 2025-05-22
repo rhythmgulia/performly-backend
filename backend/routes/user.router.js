c8d11a45d5042c6bb7e5339e7acfc8e2e57e2b83
tanya <tanyabhatnagar119@gmail.com>
2025-05-22 16:15:55 +0530

    changing the update user controller & route

--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { userValidationRules, validate } = require('../middleware/validators');
 const auth = require('../middleware/auth');
 const UserController = require('../controllers/user.controller');
+const { userValidationRules, userUpdateValidationRules, validate } = require('../middleware/validators');
 
 router.post('/signup', userValidationRules(), validate, UserController.signup);
 router.post('/login', UserController.login); // Changed from signin to login
 router.get('/profile', auth, UserController.getProfile);
-router.put('/profile', auth, userValidationRules(), validate, UserController.updateProfile);
+router.put('/profile', auth, userUpdateValidationRules(), validate, UserController.updateProfile);
 
 module.exports = router;
